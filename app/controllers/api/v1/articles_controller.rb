c52fd268f621712af75914c6fc05730b833d0256
Timothy-2017 <timothy.eubank@gmail.com>
2018-01-02 14:48:56 -0500

    random article on submit

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -6,13 +6,26 @@ module Api
         news_key = ENV["NEWS_API_KEY"]
         news_base_url = "https://newsapi.org/v2/top-headlines?sources=usa-today&apiKey=#{news_key}"
         news_response = HTTParty.get(news_base_url)
-        articles = news_response.parsed_response["articles"].slice(0, 1)
 
-        persisted_articles = articles.map do |article|
-          Article.find_or_create_by(title: article["title"], description: article["description"])
+        # byebug
+        # article = news_response.parsed_response["articles"].sample
+        # article = article["title"].concat('. ').concat(article["description"])
+
+
+        article_arr = []
+        articles = news_response.parsed_response["articles"]
+        articles.map do |article|
+          article = article["title"].concat('. ').concat(article["description"])
+          article_arr.push(article)
         end
+        # byebug
+        render json: article_arr
 
-        render json: persisted_articles
+        # articles = news_response.parsed_response["articles"].slice(0, 1)
+        # persisted_articles = articles.map do |article|
+        #   Article.find_or_create_by(title: article["title"], description: article["description"])
+        # end
+        # render json: persisted_articles
       end
 
       def create
--- app/controllers/api/v1/translations_controller.rb
+++ app/controllers/api/v1/translations_controller.rb
@@ -12,8 +12,10 @@ module Api
           render json: {translated_article: params[:english_article][:description]}
         else
           # byebug
-          article = Article.find_by(id: params[:english_article]["id"])
-          words_array = article.description.split(' ')
+          article = params["english_article"]
+          words_array = article.split(' ')
+          # article = Article.find_by(id: params[:english_article]["id"])
+          # words_array = article.description.split(' ')
           word_key = ENV["YANDEX_API_KEY"]
           language = Language.find_by(id: params[:language_id])
           word_base_url = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=#{word_key}&lang=en-#{language.code}&text="
@@ -39,3 +41,46 @@ module Api
     end
   end
 end
+
+
+# module Api
+#   module V1
+#     class TranslationsController < ApplicationController
+#
+#       def index
+#         render json: Translation.all
+#       end
+#
+#       def create
+#         if (params[:language_id]).to_i === 5
+#           # byebug
+#           render json: {translated_article: params[:english_article][:description]}
+#         else
+#           # byebug
+#           article = Article.find_by(id: params[:english_article]["id"])
+#           words_array = article.description.split(' ')
+#           word_key = ENV["YANDEX_API_KEY"]
+#           language = Language.find_by(id: params[:language_id])
+#           word_base_url = "https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=#{word_key}&lang=en-#{language.code}&text="
+#
+#           translated_article = words_array.map do |word|
+#             # byebug
+#             word = word.gsub(/[^0-9A-Za-z]/, '')
+#             word_url = word_base_url + word
+#             word_response = HTTParty.get(word_url)
+#
+#             if !(word_response.parsed_response["def"].length === 0)
+#               newWord = Word.find_or_create_by(text: word, pos: word_response.parsed_response["def"][0]["pos"])
+#               translated_word = word_response.parsed_response["def"][0]["tr"][0]["text"]
+#               newTranslation = Translation.find_or_create_by(text: translated_word, language_id: language.id, word_id: newWord.id)
+#               newTranslation.text
+#             else
+#               word
+#             end
+#           end
+#           render json: {translated_article: translated_article.join(" ")}
+#         end
+#       end
+#     end
+#   end
+# end
